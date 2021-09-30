1bac65fd364866131b173206718d1e4fc42ec01e
TheDrunkWolf21 <91485033+TheDrunkWolf21@users.noreply.github.com>
2021-09-30 22:06:31 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -49,7 +49,7 @@ while (ver == 0):
     if ((human < computer) and (win == 1)):
         print("Wow!! Your victory is coming soon")
     if ((human < computer) and (win == 2)):
-        print("Wow!! Computer scores points")
+        print("Wow! Computer scores points")
     if ((human == computer) and (win == 0)):
         print("Super draw!")
     if ((human == computer) and (win == 1)):
